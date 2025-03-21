f0f57a1b1486a493885072c669e64ac3d412d981
Murilo Santana Cardoso <123023601+murilosaca@users.noreply.github.com>
2025-03-20 23:24:34 -0300

    Add files via upload

---
+++ ProjetoSoma-main/ProjetoSoma-main/src/main/java/com/example/soma2/Soma2Application.java
@@ -0,0 +1,13 @@
+package com.example.soma2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soma2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soma2Application.class, args);
+	}
+
+}
